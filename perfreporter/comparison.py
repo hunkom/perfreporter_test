b0096f851eaba46b8f3a55d0f27c1770503f7503
Mykhailo Hunko <mykhailo_hunko@epam.com>
2019-09-18 13:29:35 +0300

    fix query

--- perfreporter/comparison.py
+++ perfreporter/comparison.py
@@ -3,6 +3,13 @@ from time import time
 import datetime
 from influxdb import InfluxDBClient
 
+SELECT_DATA_FROM_INFLUX = "SELECT last(tpsRate) as Throughput, min(responseTime) as Min, " \
+                                "max(responseTime) as Max, mean(responseTime) as Mean, " \
+                                "percentile(responseTime, 50) as pct_50, percentile(responseTime, 75) as pct_75, " \
+                                "percentile(responseTime, 90) as pct_90, percentile(responseTime, 95) as pct_95, " \
+                                "percentile(responseTime, 99) as pct_99 FROM requestsRaw WHERE " \
+                                "\"requestName\"=\'{}\' and buildID=\'{}\'"
+
 
 class Comparison(object):
     def __init__(self, arguments):
@@ -14,17 +21,9 @@ class Comparison(object):
                                 username=self.args['influx_user'], password=self.args['influx_password'],
                                 database=self.args['influx_db'])
         for request in test_results:
-            query = "SELECT last(tpsRate) as Throughput, min(responseTime) as Min, " \
-                                "max(responseTime) as Max, mean(responseTime) as Mean, " \
-                                "percentile(responseTime, 50) as pct_50, percentile(responseTime, 75) as pct_75, " \
-                                "percentile(responseTime, 90) as pct_90, percentile(responseTime, 95) as pct_95, " \
-                                "percentile(responseTime, 99) as pct_99 FROM requestsRaw WHERE " \
-                                "\"requestName\"=\'" + str(request['request_name']) + "\' and build_id=\'" \
-                                + str(request['build_id']) + "\'"
-            data = client.query(query)
+            data = client.query(SELECT_DATA_FROM_INFLUX.format(str(request['request_name']), str(request['build_id'])))
             data = list(data.get_points())
             print("**************************************")
-            print(query)
             print(request['request_name'])
             print(data)
 
