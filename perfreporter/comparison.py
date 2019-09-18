8becd127447120db339d2de835708946b3c1d20d
Mykhailo Hunko <mykhailo_hunko@epam.com>
2019-09-18 12:52:02 +0300

    get data from influx

--- perfreporter/comparison.py
+++ perfreporter/comparison.py
@@ -9,10 +9,22 @@ class Comparison(object):
         self.args = arguments
 
     def write_comparison_data_to_influx(self, test_results):
-        comparison = dict()
-        print("*******************************************")
-        for req in test_results:
-            print(req)
-            print("________________________")
-        print("*******************************************")
+        comparison = []
+        client = InfluxDBClient(self.args["influx_host"], self.args['influx_port'],
+                                username=self.args['influx_user'], password=self.args['influx_password'],
+                                database=self.args['influx_db'])
+        for request in test_results:
+            data = client.query("SELECT last(tpsRate) as Throughput, min(responseTime) as Min, "
+                                "max(responseTime) as Max, mean(responseTime) as Mean, "
+                                "percentile(responseTime, 50) as pct_50, percentile(responseTime, 75) as pct_75, "
+                                "percentile(responseTime, 90) as pct_90, percentile(responseTime, 95) as pct_95, "
+                                "percentile(responseTime, 99) as pct_99 FROM requestsRaw WHERE "
+                                "\"requestName\"=\'" + str(request['request_name']) + "\' and build_id=\'"
+                                + str(request['build_id']) + "\'")
+            data = list(data.get_points())
+            print("**************************************")
+            print(request['request_name'])
+            print(data)
+
+
         #self.write_to_comparison_db(comparison)
