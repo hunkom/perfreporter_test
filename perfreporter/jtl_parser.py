a3b94f9e91f819290482eef8ff33528f44910692
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-01-23 12:51:05 +0200

    jtl parser

--- perfreporter/jtl_parser.py
+++ perfreporter/jtl_parser.py
@@ -1,5 +1,6 @@
 import csv
 import re
+from os import path
 
 
 FIELDNAMES = 'timeStamp', 'response_time', 'request_name', "status_code", "responseMessage", "threadName", "dataType",\
@@ -11,11 +12,13 @@ class JTLParser(object):
 
     @staticmethod
     def parse_jtl():
-        path = "/tmp/reports/jmeter.xml"
+        log_file = "/tmp/reports/jmeter.jtl"
         unparsed_counter = 0
         requests = {}
+        if not path.exists(log_file):
+            return requests
         start_timestamp, end_timestamp = float('inf'), 0
-        with open(path, 'r+', encoding="utf-8") as tsv:
+        with open(log_file, 'r+', encoding="utf-8") as tsv:
             entries = csv.DictReader(tsv, delimiter=",", fieldnames=FIELDNAMES, restval="not_found")
 
             for entry in entries:
@@ -30,12 +33,17 @@ class JTLParser(object):
                             end_timestamp = int(entry['timeStamp'])
                         if entry['request_name'] not in requests:
                             data = {'request_name': entry['request_name'],
-                                    'response_time': [entry['response_time']],
-                                    'count': 1}
+                                    'response_time': [entry['response_time']]}
+                            if entry['success'] == 'true':
+                                data['OK'] = 1
+                                data['KO'] = 0
                             requests[entry['request_name']] = data
                         else:
                             requests[entry['request_name']]['response_time'].append(entry['response_time'])
-                            requests[entry['request_name']]['count'] += 1
+                            if entry['success'] == 'true':
+                                requests[entry['request_name']]['OK'] += 1
+                            else:
+                                requests[entry['request_name']]['KO'] += 1
                 except Exception as e:
                     print(e)
                     unparsed_counter += 1
@@ -43,7 +51,6 @@ class JTLParser(object):
 
         if unparsed_counter > 0:
             print("Unparsed errors: %d" % unparsed_counter)
-        duration = end_timestamp - start_timestamp
+        duration = int((end_timestamp - start_timestamp)/1000)
         print(duration)
-        print(duration/1000)
         return requests
