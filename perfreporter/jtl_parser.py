54d0125f408f1347fd33bb810208076296b21f63
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-01-23 12:08:42 +0200

    jtl parser

--- perfreporter/jtl_parser.py
+++ perfreporter/jtl_parser.py
@@ -14,17 +14,20 @@ class JTLParser(object):
         path = "/tmp/reports/jmeter.xml"
         unparsed_counter = 0
         requests = {}
+        start_timestamp, end_timestamp = int(float('inf')), 0
         with open(path, 'r+', encoding="utf-8") as tsv:
             entries = csv.DictReader(tsv, delimiter=",", fieldnames=FIELDNAMES, restval="not_found")
-            print("*******************************")
-            print(entries)
-            print("*******************************")
+
             for entry in entries:
 
                 try:
                     if entry['request_name'] != 'label':
                         if re.search(r'-\d+$', entry['request_name']):
                                 continue
+                        if start_timestamp > int(entry['timeStamp']):
+                            start_timestamp = int(entry['timeStamp'])
+                        if end_timestamp < int(entry['timeStamp']):
+                            end_timestamp = int(entry['timeStamp'])
                         if entry['request_name'] not in requests:
                             data = {'request_name': entry['request_name'],
                                     'response_time': [entry['response_time']],
@@ -40,4 +43,7 @@ class JTLParser(object):
 
         if unparsed_counter > 0:
             print("Unparsed errors: %d" % unparsed_counter)
+        duration = end_timestamp - start_timestamp
+        print(duration)
+        print(duration/1000)
         return requests
